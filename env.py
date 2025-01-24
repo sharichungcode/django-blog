0c5ca8a9045dd29055d813cde756504c5dbcff90
Shari Chung <sharichungdesign@gmail.com>
2025-01-24 00:38:44 +0000

    dabases and deployment

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_M5bE7yZcLKWd@ep-sweet-hall-a25qjgm8.eu-central-1.aws.neon.tech/spend_gig_pony_276777")
+os.environ.setdefault("SECRET_KEY", "869AE")
